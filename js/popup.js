06b989cfd35d3614fd31d0ed624f77ed2d22f31b
Arthur <arthurlitovko@gmail.com>
2020-02-21 17:21:47 +0200

    Popup script

---
+++ js/popup.js
@@ -0,0 +1,69 @@
+"use strict";
+
+let link = document.querySelector(".contacts__button"),
+  modal = document.querySelector(".contact-us"),
+  close = modal.querySelector(".modal__close");
+
+let form = modal.querySelector(".contact-us__form"),
+  name = form.querySelector("#contact-us__name"),
+  email = form.querySelector("#contact-us__email"),
+  letter = form.querySelector("#contact-us__text"),
+  isStorageSupport = true,
+  storageEmailVal,
+  storageNameVal;
+
+try {
+  storageEmailVal = localStorage.getItem("email");
+  storageNameVal = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.add("modal--show");
+  if (storageNameVal) {
+    name.value = storageNameVal;
+    if (storageEmailVal) {
+      email.value = storageEmailVal;
+    }
+    letter.focus();
+  } else {
+    name.focus();
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.remove("modal--error");
+  modal.classList.remove("modal--show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modal.classList.contains("modal--show")) {
+      evt.preventDefault();
+      modal.classList.remove("modal--error");
+      modal.classList.remove("modal--show");
+    }
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!email.value || !letter.value) {
+    evt.preventDefault();
+    //! Тут баг 🐛 😢
+    /*
+    // modal.classList.remove("modal--error");
+    // modal.offsetWidth = modal.offsetWidth;
+    */
+    modal.classList.add("modal--error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("email", email.value);
+      if (name.value) {
+        localStorage.setItem("name", name.value);
+      }
+    }
+  }
+});
