50459ca9a9c58915b7d8446c2f2a35fc76230b67
Arthur <arthurlitovko@gmail.com>
2020-02-23 00:15:17 +0200

    Price range (only thumbs)

---
+++ js/price.js
@@ -0,0 +1,59 @@
+"use strict";
+
+let priceBlock = document.querySelector(".filters--cost");
+
+const thumbRadius = priceBlock.querySelector("#price__thumb circle").r.baseVal.value;
+
+let rangeFull = priceBlock.querySelector("#price__track"),
+  rangeSelected = priceBlock.querySelector("#price__track--selected");
+const minCord = rangeFull.x1.baseVal.value - thumbRadius,
+  maxCord = rangeFull.x2.baseVal.value - thumbRadius;
+
+let minThumb = new Thumb("#price--min", "#price__output--min", rangeSelected.x1),
+  maxThumb = new Thumb("#price--max", "#price__output--max", rangeSelected.x2);
+
+function Thumb(thumb, output, rangeSelectedPoint) {
+  thumb = priceBlock.querySelector(thumb);
+  thumb.output = priceBlock.querySelector(output);
+  thumb.rangeSelectedPoint = rangeSelectedPoint;
+
+
+  thumb.getPosition = function () {
+    return this.x.baseVal.value;
+  }
+  thumb.displayPrice = function (price) {
+    this.output.value = price;
+  }
+  thumb.moveTo = function (coordinate) {
+    if (coordinate > thumb.output.max) {
+      coordinate = thumb.output.max;
+    }
+
+    if (coordinate < thumb.output.min) {
+      coordinate = thumb.output.min;
+    }
+    this.rangeSelectedPoint.baseVal.value = coordinate + thumbRadius;
+    this.x.baseVal.value = coordinate;
+    this.displayPrice(Math.floor(coordinate / maxCord * maxPrice));
+  }
+
+  thumb.onmousedown = function (event) {
+    event.preventDefault;
+    let startPos = event.clientX;
+
+    let thisThumb = this;
+    document.onmousemove = function (event) {
+      let shift = event.clientX - startPos;
+      startPos = event.clientX;
+      thisThumb.moveTo(thisThumb.getPosition() + shift);
+    }
+
+    document.onmouseup = function () {
+      document.onmousemove = null;
+      document.onmouseup = null;
+    }
+  }
+
+  return thumb;
+
+}
