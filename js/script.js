16066522cedb753c482b10ada2ab624c68838852
Arthur <arthurlitovko@gmail.com>
2020-02-24 18:34:57 +0200

    First try

--- js/script.js
+++ js/script.js
@@ -1,12 +1,1 @@
 "use strict";
-
-let slider = document.querySelector(".slider"),
-  slides = slider.querySelectorAll(".slider__item"),
-  sliderToggles = slider.querySelectorAll(".slider__toggle");
-
-for (let i = 0; i < sliderToggles.length; i++) {
-  sliderToggles[i].addEventListener("change", function () {
-    let path = "#" + slides[i].id;
-    document.location.href = path;
-  })
-}
