2546d0467d69e2d4ad795fb71b2ded9f74558cc5
Arthur <arthurlitovko@gmail.com>
2020-02-24 17:47:03 +0200

    Fixed dynamic slider toggles creation for IE 😒

--- js/slider.js
+++ js/slider.js
@@ -2,25 +2,34 @@
 
 let slider = document.querySelector(".slider"),
   slides = slider.querySelectorAll(".slider__item"),
-  sliderToggles = slider.querySelector(".slider__toggles"),
-  sliderToggleTemplate = sliderToggles.querySelector("#toggle__template").content;
+  sliderToggles = slider.querySelector(".slider__toggles");
+// sliderToggleTemplate = sliderToggles.querySelector("#toggle__template").content;
 
 for (let index = 0; index < slides.length; index++) {
-  let newToggle = sliderToggleTemplate.cloneNode(true),
-    input = newToggle.querySelector("input"),
-    label = newToggle.querySelector("label");
+  let newToggle = document.createElement("li"),
+    outputIndex = index + 1;
 
-  input.id += (index + 1);
-  label.htmlFor += (index + 1);
-  label.textContent += (` №${index + 1}`)
-
-  if (index == 0) {
-    input.checked = true;
+  let toggleInput = document.createElement("input");
+  toggleInput.type = "radio";
+  toggleInput.name = "slider";
+  toggleInput.id = "slider__toggle--" + outputIndex;
+  toggleInput.classList.add("slider__toggle", "visually-hidden")
+  if (outputIndex == 1) {
+    toggleInput.checked = true;
   }
+  newToggle.appendChild(toggleInput);
 
-  input.addEventListener("change", function () {
+
+  let toggleLabel = document.createElement("label");
+  toggleLabel.htmlFor = toggleInput.id;
+  toggleLabel.textContent = "Перейти к  слайду №" + outputIndex;
+  toggleLabel.classList.add("slider__label");
+  newToggle.appendChild(toggleLabel);
+
+  toggleInput.addEventListener("change", function () {
     let path = "#" + slides[index].id;
     document.location.href = path;
   })
+
   sliderToggles.appendChild(newToggle);
 }
